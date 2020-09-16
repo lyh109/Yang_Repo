a5c301d420e125b73b453eed2509110459122b53
lyh109 <meekk1009@gmail.com>
2020-09-16 23:41:18 +0900

    new upload

---
+++ Class Content/0914/0914.py
@@ -0,0 +1,31 @@
+from pico2d import* 
+
+open_canvas()
+
+gra = load_image('../res/grass.png')
+cha = load_image('../res/animation_sheet.png')
+
+gra.draw_now(400, 30)
+cha.draw_now(400, 85)
+
+x = 0
+frame_index = 0
+action = 0
+
+while(x < 800):
+    clear_canvas()
+    gra.draw(400, 30)
+    cha.clip_draw(100 * frame_index, 100 * action, 100, 100, x, 85)
+    update_canvas()
+
+    get_events()
+    
+    x+= 2
+    if (x % 100 == 0):
+        action = (action + 1) % 4
+    frame_index = (frame_index + 1) % 8
+    delay(0.01)
+
+delay(2)
+
+close_canvas()
