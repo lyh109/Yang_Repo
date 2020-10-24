46187436921ef88dbbfabe92e4d1aa863ccdc463
lyh109 <meekk1009@gmail.com>
2020-10-25 01:33:48 +0900

    쿠키 자동 달리기

--- Term Project/main.py
+++ Term Project/main.py
@@ -3,6 +3,21 @@ from pico2d import *
 SCREEN_WIDTH = 1136
 SCREEN_HEIGHT = 636
 
+open_canvas(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+class Cookie:
+    ACTIONS = ['Run', 'Jump', 'Double Jump', 'Bump', 'Dead']
+    def __init__(self):
+        self.x, self.y = 150, 208
+        self.image = load_image('res/Brave_Cookie.png')
+        self.action = 0
+        self.speed = 50
+        self.dx = 2.5
+        self.fidx = 0
+        self.fidy = 2
+
+Player = Cookie()
+
 def handle_events():
     global running
     evts = get_events()
@@ -12,8 +27,9 @@ def handle_events():
         elif e.type == SDL_KEYDOWN:
             if e.key == SDLK_ESCAPE:
                 running = False
-
-open_canvas(SCREEN_WIDTH, SCREEN_HEIGHT)
+            elif e.key == SDLK_SPACE:
+                Player.action = 1
+                Player.fidy = 6
 
 flow = 0
 nf = 0
@@ -26,6 +42,8 @@ black_screen = load_image('res/bb.png')
 alpha = 0.0
 alpha_up = True
 run = True
+
+jump_up = True
 
 while True:
     handle_events()
@@ -40,18 +58,32 @@ while True:
     if not run:
         if alpha_up:
             alpha += 0.001
+
+            Player.x += Player.dx
+            Player.image.clip_draw(2 + (Player.fidx * 272), 1634 - (Player.fidy * 272), 270, 270, Player.x, Player.y, 180, 180)
+            Player.fidx = (Player.fidx + 1) % 4
+
             if alpha >= 1.0:
                alpha_up = False 
                alpha = 1.0
                if stage < len(images) - 1:
                     stage += 1
                cur_image = images[stage]
+               Player.x = -270
+               Player.y = 208
         else:
             alpha -= 0.001
+
+            Player.image.clip_draw(2 + (Player.fidx * 272), 1634 - (Player.fidy * 272), 270, 270, Player.x, Player.y, 180, 180)
+            Player.fidx = (Player.fidx + 1) % 4
+
+            if Player.x < 150:
+                Player.x += 0.4
+            
             if alpha <= 0.0:
                 alpha_up = True
                 run = True
-
+        
         black_screen.opacify(alpha)
         black_screen.draw(black_screen.w / 2, black_screen.h / 2)
 
@@ -60,14 +92,29 @@ while True:
         if flow >= cur_image.w:
             flow = 0
             nf += 1
-            if nf % 4 == 0:
+            if (nf + stage) % 4  == 0:
                 nf = 0
                 flow = 0
                 if stage < len(images) - 1:
                     run = False
 
-    print(flow)
+        Player.image.clip_draw(2 + (Player.fidx * 272), 1634 - (Player.fidy * 272), 270, 270, Player.x, Player.y, 180, 180)
+        if Player.action == 0:
+            Player.fidx = (Player.fidx + 1) % 4
+        elif Player.action == 1:
+            if Player.y % 5 == 30:
+                Player.fidx = (Player.fidx + 1) % 5
+            # if jump_up:
+            #    Player.y += 1
+            #    if Player.y >= 350:
+            #        jump_up = False
+            # else:
+            #    Player.y -= 1
+            #    if Player.y <= 208:
+            #        jump_up = True
+            #        Player.action = 0
 
+    print(flow)
     update_canvas()
 
 close_canvas()
