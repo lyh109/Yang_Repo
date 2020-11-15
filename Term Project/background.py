17a959eed96c047ce0e53d1cac14434d443c3cb6
lyh109 <meekk1009@gmail.com>
2020-11-15 18:44:22 +0900

    배경 업데이트

--- Term Project/background.py
+++ Term Project/background.py
@@ -1,10 +1,20 @@
 import gfw
 
 class Background:
-    def __init__(self):
-        self.background = gfw.Sprite('./res/bk1.png')
-        self.background.scale_x = 2.0
-        self.background.scale_y = 2.0
-        self.background.origin_x = 0.0
-        self.background.origin_y = 0.0
-        gfw.renderer.add(self.background)
+    def __init__(self, image_path):
+        self.repeat_count = 0
+        self.speed = 0.0
+
+        self.back_pieces = [ gfw.Sprite(image_path), gfw.Sprite(image_path) ] # TODO: repeat만큼 loop 돌리자
+        for i in range(0, self.repeat_count):
+            self.back_pieces[i].scale_x = 2.0
+            self.back_pieces[i].scale_y = 2.0
+            self.back_pieces[i].origin_x = -i
+            self.back_pieces[i].origin_y = 0.0
+            gfw.renderer.add(self.back_pieces[i])
+
+    def update(self):
+        for i in self.back_pieces:
+            i.origin_x += gfw.delta_time * 0.5
+            if i.origin_x >= 1.0:
+                i.origin_x -= 2.0
--- Term Project/main_state.py
+++ Term Project/main_state.py
@@ -4,11 +4,15 @@ from background import Background
 
 class MainState:
     def init(self):
-        self.background = Background()
+        self.background = Background('./res/bk2.png')
+        self.background.repeat_count = 2
+        self.background.speed = 0.5
+
         self.cookie = Cookie()
 
     def update(self):
         self.cookie.update()
+        self.background.update()
 
         if gfw.eh.get_mouse_button(gfw.eh.LBUTTON):
             print(gfw.eh.mouse_pos[0])
