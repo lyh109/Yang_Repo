525732609b1bbd6cb694bc5063fd23ca9512b6e0
DESKTOP-P6B99LV\meekk <meekk1009@gmail.com>
2020-11-23 21:32:09 +0900

    배경 이미지 잘려서 출력되는 부분이 없도록 수정, 필요없는 리소스 삭제

--- Term Project/main_state.py
+++ Term Project/main_state.py
@@ -4,13 +4,19 @@ from background import Background
 
 class MainState:
     def init(self):
-        self.background = Background('./res/bk1-1.png', 2, 0.15)
-        self.background = Background('./res/bk1-2.png', 2, 0.3)
+        self.background1 = Background('./res/bk11-1.png', 2, 0.1)
+        self.background2 = Background('./res/bk11-2.png', 3, 0.2)
+        self.background3 = Background('./res/bk11-3.png', 2, 0.4)
+        self.background4 = Background('./res/bk11-4.png', 2, 0.6)
+
         self.cookie = Cookie()
 
     def update(self):
         self.cookie.update()
-        self.background.update()
+        self.background1.update()
+        self.background2.update()
+        self.background3.update()
+        self.background4.update()
 
         if gfw.eh.get_mouse_button(gfw.eh.LBUTTON):
             print(gfw.eh.mouse_pos[0])
