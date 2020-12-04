06ca625a63decf26b67f427a43dfb4f9faa4af84
DESKTOP-KJDL4HB\USER <meekk1009@gmail.com>
2020-12-04 16:00:02 +0900

    background.py 수정

--- Term Project/background.py
+++ Term Project/background.py
@@ -2,15 +2,14 @@ from pico2d import *
 import gfw
 
 class Background:
-    def __init__(self, image_path, repeat_count, speed):
+    def __init__(self, image_path, repeat_count, speed, scale, ):
         self.speed = speed
-        self.bgm = load_music('./res/sound/bgm_gameplay.ogg')
-        self.bgm.repeat_play()
+        self.repeat_count = repeat_count
 
         self.back_pieces = [  gfw.Sprite(image_path) for i in range(repeat_count) ]
         for i in range(0, repeat_count):
-            self.back_pieces[i].scale_x = 2.0
-            self.back_pieces[i].scale_y = 2.0
+            self.back_pieces[i].scale_x = scale
+            self.back_pieces[i].scale_y = scale
             self.back_pieces[i].origin_x = -i
             self.back_pieces[i].origin_y = 0.0
             gfw.renderer.add(self.back_pieces[i])
@@ -19,4 +18,4 @@ class Background:
         for i in self.back_pieces:
             i.origin_x += gfw.delta_time * self.speed
             if i.origin_x >= 1.0:
-                i.origin_x -= 2.0
+                i.origin_x -= self.repeat_count
