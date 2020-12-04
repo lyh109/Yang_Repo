4c16e7cfb93d4e2bfcfb97177b7f4b9009d0e231
DESKTOP-KJDL4HB\USER <meekk1009@gmail.com>
2020-12-04 16:00:50 +0900

    젤리 오브젝트 기본 구현

---
+++ Term Project/item.py
@@ -0,0 +1,47 @@
+import gfw
+
+class Item:
+    def __init__(self, data):
+        self.frame_count = 0
+        self.kind = int(data['name'][1])
+
+        if data['name'] == 'j0' or data['name'] == 'j1':
+            self.spr = gfw.Sprite('./res/basic_jellies.png')
+            self.frame_count = 3
+            self.spr.cell_image_width = 38
+            self.spr.cell_index_x = self.kind * self.frame_count
+        else:
+            self.spr = gfw.Sprite('./res/bear_jellies.png')
+            if self.kind % 2 == 0:
+                self.frame_count = 1
+            else:
+                self.frame_count = 2
+
+            if self.kind == 2:
+                self.spr.cell_index_x = 0
+            elif self.kind == 3:
+                self.spr.cell_index_x = 1
+            elif self.kind == 4:
+                self.spr.cell_index_x = 3
+            elif self.kind == 5:
+                self.spr.cell_index_x = 4
+            elif self.kind == 6:
+                self.spr.cell_index_x = 6
+            elif self.kind == 7:
+                self.spr.cell_index_x = 7
+            elif self.kind == 8:
+                self.spr.cell_index_x = 9
+            elif self.kind == 9:
+                self.spr.cell_index_x = 10
+
+            self.spr.cell_image_width = 55
+        
+        self.spr.cell_image_height = 51       
+        self.spr.x = data["x"]
+        self.spr.y = data["y"]
+        self.spr.is_clip_image = True
+        self.spr.padding_size = 2
+        self.spr.cell_index_y = 0
+        gfw.renderer.add(self.spr)
+
+        self.score = 0.0
--- Term Project/main_state.py
+++ Term Project/main_state.py
@@ -1,15 +1,31 @@
+from gfw import sprite
+from pico2d import *
+import json
+
 import gfw
 from cookie import Cookie
 from background import Background
+from item import Item
 
 class MainState:
     def init(self):
-        self.background1 = Background('./res/bk11-1.png', 2, 0.1)
-        self.background2 = Background('./res/bk11-2.png', 3, 0.2)
-        self.background3 = Background('./res/bk11-3.png', 2, 0.4)
-        self.background4 = Background('./res/bk11-4.png', 2, 0.6)
+        # self.bgm = load_music('./res/sound/bgm_gameplay.ogg')
+        # self.bgm.repeat_play()
+        
+        self.background1 = Background('./res/bk/bk11-1.png', 2, 0.1, 2)
+        self.background2 = Background('./res/bk/bk11-2.png', 3, 0.2, 2)
+        self.background3 = Background('./res/bk/bk11-3.png', 2, 0.4, 2)
+        self.background4 = Background('./res/bk/bk11-4.png', 2, 0.6, 2)
+        self.tile = Background('./res/tile/tile11.png', 12, 3.0, 1)
         
         self.cookie = Cookie()
+
+        objects = None
+        with open('./res/data/objects.json') as f:
+            objects = json.load(f)
+
+        for o in objects:
+            item = Item(o)
 
     def update(self):
         self.cookie.update()
@@ -17,9 +33,7 @@ class MainState:
         self.background2.update()
         self.background3.update()
         self.background4.update()
-
-        if gfw.eh.get_mouse_button(gfw.eh.LBUTTON):
-            print(gfw.eh.mouse_pos[0])
+        self.tile.update()
 
 if __name__ == '__main__':
     gfw.init(MainState())
